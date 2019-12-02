2b8fa1cfe03d615083f346507c476a86f52dfd9f
VictoriaVend <42913694+VictoriaVend@users.noreply.github.com>
2019-12-02 23:54:59 +0200

    lost

--- src/main/java/telran/java30/forum/configuration/security/SecurityConfiguration.java
+++ src/main/java/telran/java30/forum/configuration/security/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package telran.java30.forum.configuration.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
